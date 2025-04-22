5d68682a6464173bc400b4a1ec9947c33a01988f
Cendon19 <a.cendon19@gmail.com>
2025-04-22 23:45:03 +0200

    feature: finishing first section (intro)

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,15 @@ export default function Home() {
       >
         <div className="container mx-auto h-screen flex items-center flex flex-col w-full">
           <div className="flex flex-col justify-center items-center gap-5 flex-1 ">
-            <h1 className="text-4xl sm:text-7xl text-white uppercase font-bold">
+            <h1 className="text-4xl sm:text-7xl text-white uppercase font-bold cursor-default">
               Adrián Cendón
             </h1>
             <div className="text-white uppercase font-bold">
               <HyperText>FullStack Developer</HyperText>
             </div>
-            <div className="flex justify-center items-center gap-5 text-white uppercase font-bold">
-              <Github />
-              <Linkedin />
+            <div className="flex justify-center items-center gap-5 text-white uppercase font-bold cursor-default">
+              <a className="transform transition-transform duration-300 hover:scale-125" href="https://github.com/Cendon19" target="_blank"><Github /></a>
+              <a className="transform transition-transform duration-300 hover:scale-125" href="https://www.linkedin.com/in/adri%C3%A1n-cend%C3%B3n-g%C3%B3mez-7a028519a/" target="_blank"><Linkedin /></a>
             </div>
           </div>
         </div>
